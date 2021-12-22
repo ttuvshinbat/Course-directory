744c1baed606cfb2624ea0780f2083fec8e1c603
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2021-12-22 10:46:19 +0800

    Updates

---
+++ precache-manifest.b8ec34a791773b9653445c03a5a7e4af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "529af2cd8c38c3486619",
+    "url": "/ttuvshinbat/Course-directory.git/static/css/main.8f766f1a.chunk.css"
+  },
+  {
+    "revision": "529af2cd8c38c3486619",
+    "url": "/ttuvshinbat/Course-directory.git/static/js/main.529af2cd.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/ttuvshinbat/Course-directory.git/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "caa0e602c5cf594638db",
+    "url": "/ttuvshinbat/Course-directory.git/static/js/runtime~main.caa0e602.js"
+  },
+  {
+    "revision": "c3f1c2df918a5f83a539805e244aab34",
+    "url": "/ttuvshinbat/Course-directory.git/index.html"
+  }
+];
